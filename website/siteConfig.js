b12bb56ed9d92c6434caab091346463aebacfc51
Yossi Rizgan <yosriz@gmail.com>
2018-06-19 20:29:11 +0300

    fix site urls

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'Kin Developer Platform' /* title for your website */,
   tagline: 'A website for testing',
-  url: 'https://kinecosystem.org/' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  url: 'https://kinecosystem.github.io' /* your website url */,
+  baseUrl: '/kin-core-docs/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
